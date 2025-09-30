fea03ab90ff565c9ba874a31ccca29688b26cb82
한승완 <hseungwan8@gmail.com>
2025-09-30 15:28:43 +0900

    feat(update): 앱 업데이트 다이얼로그 기능 추가

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:device_info_plus/device_info_plus.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:app_version_update/app_version_update.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_badge/flutter_app_badge.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:one_by_one/common/app_page_url.dart';
@@ -13,6 +12,7 @@ import 'package:one_by_one/common/pref/app_pref.dart';
 import 'package:one_by_one/screen/webview_screen.dart';
 import 'package:one_by_one/theme/app_theme.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// 앱 기본 로직 처리 App Widget
 class App extends StatefulWidget {
@@ -71,6 +71,9 @@ class _AppState extends State<App> {
     /// 푸시 알림 뱃지 삭제
     _deleteBadge();
 
+    /// 앱 버전 체크
+    _checkAppVersion();
+
     super.initState();
   }
 
@@ -112,7 +115,6 @@ Future<void> _setVersion() async {
 void getMyDeviceToken() async {
     String? token = await FirebaseMessaging.instance.getToken();
     await Prefs.fcmToken.set(token!);
-    CommonUtil.logger.d("토큰 설정 완료 : ${Prefs.fcmToken.get()}");
     print("토큰 설정 완료 : ${Prefs.fcmToken.get()}");
 }
 
@@ -123,57 +125,58 @@ Future<void> _deleteBadge() async {
   CommonUtil.logger.d("뱃지 삭제");
 }
 
-/// TODO : 배포 시 스토어 URL 적용
-// Future<void> _checkAppVersion() async {
-//   PackageInfo packageInfo = await PackageInfo.fromPlatform();
-//   await AppVersionUpdate.checkForUpdates(
-//     appleId: '0000000000',
-//     playStoreId: packageInfo.packageName,
-//     country: 'kr',
-//   ).then((result) async {
-//     if (result.canUpdate!) {
-//       await Get.dialog(
-//         AlertDialog(
-//           title: const Text('원바원'),
-//           content: Text(
-//             '${result.storeVersion} 업데이트가 존재합니다.\n업데이트 취소를 누르시면 기능에\n제한이 있을 수 있습니다.',
-//             softWrap: true,
-//             overflow: TextOverflow.visible,
-//           ),
-//           actions: [
-//             TextButton(
-//               onPressed: () {
-//                 Get.back();
-//               },
-//               child: const Text('취소'),
-//             ),
-//             TextButton(
-//               onPressed: () {
-//                 String storeUrl = getStoreUrlValue(
-//                     packageInfo.packageName, packageInfo.appName);
-//                 if (storeUrl == "") return;
-//                 launchUrl(
-//                   WebUri(storeUrl),
-//                   mode: LaunchMode.externalApplication,
-//                 );
-//                 Get.back();
-//               },
-//               child: const Text('확인'),
-//             ),
-//           ],
-//         ),
-//       );
-//     }
-//   });
-// }
+/// 앱 버전 체크
+Future<void> _checkAppVersion() async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  await AppVersionUpdate.checkForUpdates(
+    appleId: '6744432824',
+    playStoreId: packageInfo.packageName,
+    country: 'kr',
+  ).then((result) async {
+    print("앱 버전 체크 결과 >> ${result.canUpdate}, 스토어 버전 > ${result.storeVersion}, 현재 버전 > ${packageInfo.version}");
+    if (result.canUpdate!) {
+      await Get.dialog(
+        AlertDialog(
+          title: const Text('원바원'),
+          content: Text(
+            '${result.storeVersion} 업데이트가 존재합니다.\n업데이트 취소를 누르시면 기능에\n제한이 있을 수 있습니다.',
+            softWrap: true,
+            overflow: TextOverflow.visible,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: const Text('취소'),
+            ),
+            TextButton(
+              onPressed: () {
+                String storeUrl = getStoreUrlValue(
+                    packageInfo.packageName, packageInfo.appName);
+                if (storeUrl == "") return;
+                launchUrl(
+                  WebUri(storeUrl),
+                  mode: LaunchMode.externalApplication,
+                );
+                Get.back();
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        ),
+      );
+    }
+  });
+}
 
-/// TODO : 배포 시 스토어 URL 적용
-// String getStoreUrlValue(String packageName, String appName) {
-//   if (Platform.isAndroid) {
-//     return "https://play.google.com/store/apps/details?id=$packageName";
-//   } else if (Platform.isIOS) {
-//     return "http://apps.apple.com/kr/app/$appName/id0000000000";
-//   } else {
-//     return "";
-//   }
-// }
+/// 스토어 URL 반환
+String getStoreUrlValue(String packageName, String appName) {
+  if (Platform.isAndroid) {
+    return "https://play.google.com/store/apps/details?id=$packageName";
+  } else if (Platform.isIOS) {
+    return "http://apps.apple.com/kr/app/$appName/id6744432824";
+  } else {
+    return "";
+  }
+}
